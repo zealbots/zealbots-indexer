91897413a5c7571631b416e3c0a63eb5c765c044
MrVKY <vignesh101012@gmail.com>
2020-04-02 22:04:28 +0530

    Sent url from bg script to pupup script

--- add-on/script.js
+++ add-on/script.js
@@ -1,3 +1,17 @@
+var title,doi,url;
+
+function handleResponse(message){
+    alert(message.response);
+}
+function handleError(error) {
+    console.log(`Error: ${error}`);
+  }
+
 document.getElementById('notifiyMe').addEventListener('click', function(){
-        alert("Hello");
+    
+    url = browser.runtime.sendMessage({
+        requesting : "url"
     });
+    
+    url.then(handleResponse, handleError);
+});

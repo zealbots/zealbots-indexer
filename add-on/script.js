0288192796f061a7e284b926634e0ff5a39216b7
Aswin Gnanaprakash <aswin@chadura.com>
2020-04-07 02:14:00 +0530

    lines reduced

--- add-on/script.js
+++ add-on/script.js
@@ -1,4 +1,3 @@
-
 var title,url;
 var urlBox;
 
@@ -12,74 +11,54 @@ document.getElementById('submit').addEventListener('click', function(){
 
 });
 
-
-
 //scraping section
 
 browser.runtime.onMessage.addListener(function(request, sender) {
     
     if (request.action == "getSource") {
-      
-
       var doc = new DOMParser().parseFromString(request.source, "text/html");
 
       url = String(doc.querySelector('meta[property="og:url"]').content);
       let urlSplit = url.split('/');
-      if( urlSplit[2] == 'www.nature.com' )
-      {
-          if(urlSplit[3] == 'articles'){
-              if(urlSplit[4] != ''){
 
-
-
-                    //for getting multiple keywords
-                    var elements = doc.getElementsByName('dc.subject');
-                    var keys = '';
-                    for(var i=0; i<elements.length; i++) {
-                        keys += elements[i].content + ", ";
-                    }
-                    if(doc.title != null){
-                      document.querySelector('#title').value = doc.title;
-                      removeAttr();
-                    }                     
-
-                    if(doc.querySelector('meta[name="DOI"]') != null)
-
-                      document.querySelector('#doi').value = doc.querySelector('meta[name="DOI"]').content;
-
-                    if(doc.querySelector('meta[name="dc.creator"]') != null)
-
-                      document.querySelector('#authors').value = doc.querySelector('meta[name="dc.creator"]').content;
-
-                    if(doc.querySelector('meta[name="dc.description"]') != null)
-                    
-                      document.querySelector('#abstract').value = doc.querySelector('meta[name="dc.description"]').content;
-                    
-                    if(keys != null)
-
-                      document.querySelector('#keywords').value = keys;
-
-                    if(doc.querySelector('meta[name="dc.publisher"]') != null)
-
-                      document.querySelector('#journal_name').value = doc.querySelector('meta[name="dc.publisher"]').content;
-
-                    if(doc.querySelector('span[itemprop="issn"]').innerText != '')
-
-                      document.querySelector('#journal_ISSN').value = doc.querySelector('span[itemprop="issn"]').innerText;
-
-                    if(doc.querySelector('meta[property="og:url"]') != null)
-
-                      document.querySelector('#url').value = doc.querySelector('meta[property="og:url"]').content;
-                    
-                    message.innerText = ''
-
-                  }  
-              }
-                   
+      // nature block
+      if( urlSplit[2] == 'www.nature.com' &&  urlSplit[3] == 'articles') {
+          //for getting multiple keywords
+          var elements = doc.getElementsByName('dc.subject');
+          var keys = '';
+          for(var i=0; i<elements.length; i++) {
+              keys += elements[i].content + ", ";
           }
-      }
+          if(doc.title != null) {
+            document.querySelector('#title').value = doc.title;
+            removeAttr();
+          }                     
 
-   
+          if(doc.querySelector('meta[name="DOI"]') != null)
+            document.querySelector('#doi').value = doc.querySelector('meta[name="DOI"]').content;
+
+          if(doc.querySelector('meta[name="dc.creator"]') != null)
+            document.querySelector('#authors').value = doc.querySelector('meta[name="dc.creator"]').content;
+
+          if(doc.querySelector('meta[name="dc.description"]') != null)
+            document.querySelector('#abstract').value = doc.querySelector('meta[name="dc.description"]').content;
+          
+          if(keys != null)
+            document.querySelector('#keywords').value = keys;
+
+          if(doc.querySelector('meta[name="dc.publisher"]') != null)
+            document.querySelector('#journal_name').value = doc.querySelector('meta[name="dc.publisher"]').content;
+
+          if(doc.querySelector('span[itemprop="issn"]').innerText != '')
+            document.querySelector('#journal_ISSN').value = doc.querySelector('span[itemprop="issn"]').innerText;
+
+          if(doc.querySelector('meta[property="og:url"]') != null)
+            document.querySelector('#url').value = doc.querySelector('meta[property="og:url"]').content;
+          
+          message.innerText = ''
+
+         }
+      }
   });
   
   function onWindowLoad() {
